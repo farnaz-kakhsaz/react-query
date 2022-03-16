ccbf9fda7c11b8b0041ef86cacda4070b69d1312
Farnaz Kakhsaz <farnaz.kakhsaz@gmail.com>
2022-03-16 17:39:06 +0330

    add pagination

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,27 +1,46 @@
+import { useState } from "react";
 import { useQuery } from "react-query";
 import Character from "./Character";
 
 export default function Characters() {
-  const fetchCharacters = async () => {
-    const response = await fetch("https://rickandmortyapi.com/api/character");
+  const [page, setPage] = useState(1);
+  const fetchCharacters = async ({ queryKey }) => {
+    const response = await fetch(
+      `https://rickandmortyapi.com/api/character?page=${queryKey[1]}`
+    );
     return response.json();
   };
 
-  const { data, status } = useQuery("characters", fetchCharacters);
+  const { data, isLoading, isError, isPreviousData } = useQuery(
+    ["characters", page],
+    fetchCharacters,
+    {
+      keepPreviousData: true,
+    }
+  );
 
-  if (status === "loading") {
+  if (isLoading) {
     return <div>Loading...</div>;
   }
 
-  if (status === "error") {
+  if (isError) {
     return <div>Error</div>;
   }
 
   return (
     <div className="characters">
       {data.results.map((character, index) => (
-        <Character character={character} />
+        <Character character={character} key={character.id} />
       ))}
+      <button onClick={() => setPage(page - 1)} disabled={page <= 1}>
+        Previous
+      </button>
+      <button
+        onClick={() => setPage(page + 1)}
+        disabled={isPreviousData || !data.info.next}
+      >
+        Next
+      </button>
     </div>
   );
 }
